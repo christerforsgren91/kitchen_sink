c13cb2377c4a98248b6c60ab7754fa90d70b7d26
kim_haaga <kim.haaga@live.se>
2021-05-14 18:24:21 +0200

    Install cypress as dev dependency

---
+++ js/redux/cypress/integration/userCanSeeLayout.feature.js
@@ -0,0 +1,12 @@
+describe('User can see content of page', () => {
+  describe('successfully', () => {
+    beforeEach(() => {
+      cy.visit('/')
+    })
+
+    it('is expected to show counter', () => {
+    
+    });
+  })
+  
+})
