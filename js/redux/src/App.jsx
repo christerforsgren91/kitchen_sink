f05027c8be56e48cd9aac3556b43674d3a30e62a
kim_haaga <kim.haaga@live.se>
2021-05-15 14:13:13 +0200

    Add materialize add material icon

--- js/redux/src/App.jsx
+++ js/redux/src/App.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
 const App = () => {
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  });
   return (
     <div>
       <h1>Hi</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
