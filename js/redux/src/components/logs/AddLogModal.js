710a43c319937d254f8e4cb81e3ae5230813d57e
Christer <christerforsgren91@gmail.com>
2021-05-29 23:51:49 +0200

    Add add log functionality

--- js/redux/src/components/logs/AddLogModal.js
+++ js/redux/src/components/logs/AddLogModal.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addLog } from '../../actions/logActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddLogModal = () => {
+const AddLogModal = ({ addLog }) => {
   const [message, setMessage] = useState('');
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState('');
@@ -10,7 +13,16 @@ const AddLogModal = () => {
     if (message === '' || tech === '') {
       M.toast({ html: 'Please enter a message and tech' });
     } else {
-      console.log(message, tech, attention);
+      const newLog = {
+        message,
+        attention,
+        tech,
+        date: new Date(),
+      };
+
+      addLog(newLog);
+
+      M.toast({ html: `Log added by ${tech}` });
 
       // Clear Fields
       setMessage('');
@@ -20,45 +32,46 @@ const AddLogModal = () => {
   };
 
   return (
-    <div id='add-log-modal' className='modal' style={modalStyle}>
-      <div className='modal-content'>
+    <div id="add-log-modal" className="modal" style={modalStyle}>
+      <div className="modal-content">
         <h4>Enter System Log</h4>
-        <div className='row'>
-          <div className='input-field'>
+        <div className="row">
+          <div className="input-field">
             <input
-              type='text'
-              name='message'
+              type="text"
+              name="message"
               value={message}
               onChange={(event) => setMessage(event.target.value)}
             />
-            <label htmlFor='message' className='active'>
+            <label htmlFor="message" className="active">
               Log Message
             </label>
           </div>
         </div>
-        <div className='row'>
-          <div className='input-field'>
+        <div className="row">
+          <div className="input-field">
             <select
-              name='tech'
+              name="tech"
               value={tech}
-              className='browser-default'
-              onChange={(event) => setTech(event.target.value)}>
-              <option value='' disabled>
+              className="browser-default"
+              onChange={(event) => setTech(event.target.value)}
+            >
+              <option value="" disabled>
                 Select Technician
               </option>
-              <option value='John Doe'>John Doe</option>
-              <option value='Sam Smith'>Sam Smith</option>
-              <option value='Sara Wilson'>Sara Wilson</option>
+              <option value="John Doe">John Doe</option>
+              <option value="Sam Smith">Sam Smith</option>
+              <option value="Sara Wilson">Sara Wilson</option>
             </select>
           </div>
         </div>
-        <div className='row'>
-          <div className='input-field'>
+        <div className="row">
+          <div className="input-field">
             <p>
               <label>
                 <input
-                  type='checkbox'
-                  className='filled-in'
+                  type="checkbox"
+                  className="filled-in"
                   checked={attention}
                   value={attention}
                   onChange={(event) => setAttention(!attention)}
@@ -69,11 +82,12 @@ const AddLogModal = () => {
           </div>
         </div>
       </div>
-      <div className='modal-footer'>
+      <div className="modal-footer">
         <a
-          href='#!'
+          href="#!"
           onClick={onSubmit}
-          className='modal-close waves-effect waves-light blue btn'>
+          className="modal-close waves-effect waves-light blue btn"
+        >
           Enter
         </a>
       </div>
@@ -81,9 +95,13 @@ const AddLogModal = () => {
   );
 };
 
+AddLogModal.propTypes = {
+  addLog: PropTypes.func.isRequired,
+};
+
 const modalStyle = {
   width: '75%',
   height: '75%',
 };
 
-export default AddLogModal;
+export default connect(null, { addLog })(AddLogModal);
